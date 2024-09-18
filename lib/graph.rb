d3ec96e9a0941e57c7b8fa207b85e62d7476ce80
Monatric <nawpnawp8@gmail.com>
2024-09-19 04:43:41 +0800

    Build a graph that sets  the nodes as keys  and its coordinates as values

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,13 +1,33 @@
 # Building a graph and its functionality
 class Graph
-  attr_accessor :graph
+  attr_accessor :list, :board
 
   def initialize
-    @graph = {}
+    @list = {}
+    @board = Board.new
+    build_graph
+  end
+
+  def build_graph
+    i = 0
+    x = 0
+    board.structure.each do |row|
+      y = 0
+      row.each do |col|
+        node = Node.new
+        node.name = "V#{i}"
+        node.data = board.structure[x][y]
+        add_node(node)
+        puts "#{x} #{y}"
+        y += 1
+        i += 1
+      end
+      x += 1
+    end
   end
 
   def add_node(node_to_add)
-    graph[node_to_add] = Set.new if graph[node_to_add].nil?
-    graph
+    list[node_to_add] = Set.new if list[node_to_add].nil?
+    list
   end
 end
---
+++ lib/node.rb
@@ -0,0 +1,10 @@
+# Node
+class Node
+  attr_accessor :name, :data, :next
+
+  def initialize
+    @name = nil
+    @data = nil
+    @next = nil
+  end
+end
