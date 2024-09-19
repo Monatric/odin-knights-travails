e053780cc87f6c1ea1df69b7d149607c6979fb92
Monatric <nawpnawp8@gmail.com>
2024-09-20 01:12:38 +0800

    Test the graph

---
+++ lib/knight.rb
@@ -0,0 +1,5 @@
+# The knight piece
+class Knight
+  def initialize
+  end
+end
---
+++ lib/main.rb
@@ -0,0 +1,8 @@
+require_relative "board"
+require_relative "graph"
+require_relative "node"
+require "set"
+
+graph = Graph.new
+graph.add_node("test")
+p graph
