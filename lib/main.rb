37daaff008473e70c709a1f046fdeadf8460f57c
Monatric <nawpnawp8@gmail.com>
2024-09-27 03:17:48 +0800

    Test the knight_moves function

--- lib/main.rb
+++ lib/main.rb
@@ -4,15 +4,6 @@ require_relative "node"
 require "set"
 
 graph = Graph.new
-# graph.add_node("test")
-# puts graph.list["[0][0]"]
-# list = graph.list
-# v0 = graph.find_node_by_name("V0")
-# v1 = graph.find_node_by_data([0, 1])
-# p  v1
-# graph.add_connection(v0, v1)
-# graph.add_connection("V0", "V3")
-
-# puts graph.list[v0]
 graph.knight_moves([3, 3], [4, 3])
-# graph.print_graph_data
+# graph.knight_moves([0, 0], [7, 7])
+# graph.knight_moves([3,3], [0, 0])
